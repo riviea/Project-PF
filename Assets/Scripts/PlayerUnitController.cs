d9508351a81e2fcdc0fe3e1c01c9cfeef8969ca4
Lee dohyun <12423098+riviea@users.noreply.github.com>
2023-10-06 13:31:04 +0900

    Update PlayerUnitController.cs
    뭐 눌렀는지 체크

--- Assets/Scripts/PlayerUnitController.cs
+++ Assets/Scripts/PlayerUnitController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,16 @@ public class PlayerUnitController : UnitController
     void OnMove(InputValue input)
     {
         Vector2 vec = input.Get<Vector2>();
+
+        if (vec == Vector2.left)
+            Debug.Log("left");
+        if (vec == Vector2.right)
+            Debug.Log("right");
+        if (vec == Vector2.up)
+            Debug.Log("up");
+        if (vec == Vector2.down)
+            Debug.Log("down");
+
         CallMoveEvent(vec);
     }
 }
