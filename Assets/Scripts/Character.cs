f7010c9a7c65583c83511a4f80fd43441b50b40c
wdfdd123357@naver.com <wdfdd123357@gmail.com>
2023-11-05 08:35:15 +0900

    Update Scripts

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,30 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public abstract class Character : MonoBehaviour
 {
-    private Transform characterTransform;
-    [SerializeField] private float character_HP;
-
-    private void Awake()
-    {
-        characterTransform = GetComponent<Transform>();
-    }
+    [SerializeField] protected float character_HP;
+    [SerializeField] protected string prefab_Addr;
 
     public string GetCharacterInfo()
     {
-        /*
-        "Prefabs/Player" 부분은 임시로 한 것으로 나중에 수정해야한다(11/01)
-        1. 모든 캐릭터 Player Prefab인 것은 아니므로 나중에 수정
-
-        */
-
         string info = ((int)ClientSystem.GameObjectType.PLAYER).ToString() + "~" +
-            "Prefabs/Player" + "~" +
+            prefab_Addr + "~" +
             this.name + "~" +
-            characterTransform.localPosition.x.ToString() + "~" +
-            characterTransform.localPosition.y.ToString();
+            transform.localPosition.x.ToString() + "~" +
+            transform.localPosition.y.ToString();
+
         return info;
     }
 }
